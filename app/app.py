244cbc12a9ab01d50f2e03c10e89cd84b78a54c8
badarshahzad <badarshahzad54@gmail.com>
2018-04-04 11:20:07 +0500

    init

---
+++ app/app.py
@@ -0,0 +1,72 @@
+"""
+A basic starter app  with the Flask framework and PyMongo
+"""
+
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_HOST'] = 'localhost'
+app.config['MONGO_PORT'] = '27017'
+app.config['MONGO_DBNAME'] = "TopHits"
+
+# connect to MongoDB with the defaults
+# mongo    = PyMongo(app)
+
+mongo = PyMongo(app, config_prefix='MONGO')
+
+
+# here is the songs
+@app.route('/')
+@app.route('/songs')
+def songs():
+    songs = mongo.db.songs.find()
+    return render_template('songs.html', songs=songs)
+
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/about')
+def aboutUs():
+    return render_template("aboutus.html")
+
+
+@app.route('/contact')
+def getcontactUs():
+    return render_template("contactus.html")
+
+
+@app.route('/contact', methods=["POST"])
+def postcontactUs():
+    name = request.form['inputName']
+    email = request.form['inputEmail']
+    comments = request.form['textAreaComments']
+
+    data = {
+        "name": name,
+        "email": email,
+        "comments": comments
+    }
+
+    try:
+        mongo.db.contactUs.insert_one(data)
+        return redirect(url_for('all_comments'))
+    except err:
+        return "Inesrt Failed: Something went wrong"
+    # return jsonify(result=data);
+
+    # return render_template("contactResponse.html",result=data)
+
+
+@app.route('/comments')
+def all_comments():
+    comments = mongo.db.contactUs.find()
+    return render_template("comments.html", comments=comments)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
