4d1c23d140ee88700c418994b7a05f749e01ca31
badarshahzad <badarshahzad54@gmail.com>
2018-05-20 18:34:22 +0500

    Ready for the Demo

--- app/app.py
+++ app/app.py
@@ -16,77 +16,116 @@ app.config['MONGO_DBNAME'] = "TopHits"
 
 mongo = PyMongo(app, config_prefix='MONGO')
 
+value = 0
+@app.route('/')
+def returnValues():
+    if request.method == 'GET':
+        songs = mongo.db.songs.find()
+        return render_template('gallery.html', songs=songs)
 
-# here is the songs
 
+@app.route('/',methods=['GET','POST'])
+def getArtistValue():
 
-@app.route('/songs',methods=['GET','POST'])
-def songs():
-
+    artist_name = request.form['search']
 
     if request.method == 'GET':
         songs = mongo.db.songs.find()
-        return render_template('songs.html', songs=songs)
+        return render_template('gallery.html', songs=songs)
+
+        # search the artist with name
 
     if request.method == 'POST':
+        songs = mongo.db.songs.find({'artist_name': artist_name})
+        return render_template('gallery.html', songs=songs)
 
-        #is_artisti_include
-        value = request.form.get("is_include_artist")
-
-
-        #search the artist with name
-        artist_name = request.form['search']
-
-        if value :
-            songs = mongo.db.songs.find({'artist_name': "Eminem"})
-        else:
-            songs = mongo.db.songs.find({'artist_name': artist_name})
-        return render_template('songs.html', songs=songs)
+    if artist_name == "":
+        songs = mongo.db.songs.find({'artist_name': artist_name})
+        return render_template('gallery.html', songs=songs)
 
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template("home.html")
+@app.route('/query/<number>')
+def aboutUs(number):
+        number = int(number)
+
+        #== == == == == == == == == == == == == == == == == == == == == =
+        # what are the 5 songs with lowest tempi across all year?
+        #----------------------------------------------------------------
+        # db.songs.find().sort({tempo: +1}).limit(5).pretty()
+
+        if number == 1 :
+            songs = mongo.db.songs.find().sort([('tempo', 1)]).limit(5)
+            return render_template('gallery.html', songs=songs)
+
+        # == == == == == == == == == == == == == == == == == == == == == =
+        # what are the 5 songs witgh minimum sound quality?
+        # ----------------------------------------------------------------
+        #  db.songs.find().sort({Sound_quality:-1}).limit(5).pretty()
+
+        elif number == 2 :
+            songs = mongo.db.songs.find().sort([('Sound_quality', -1)]).limit(5)
+            return render_template('gallery.html', songs=songs)
+
+        # == == == == == == == == == == == == == == == == == == == == == =
+        # what are the top 10 songs with maximum energy across all years?
+        # ----------------------------------------------------------------
+        #  db.songs.find().sort({energy:-1}).limit(10).pretty()
+
+        elif number == 3 :
+            songs = mongo.db.songs.find().sort([('energy', -1)]).limit(10)
+            return render_template('gallery.html', songs=songs)
+
+        # == == == == == == == == == == == == == == == == == == == == == =
+        # what are the 10 most danceable songs from the 2000's
+        # ----------------------------------------------------------------
+        #  db.songs.find().sort({danceability:-1},{"years":"2000s"}).limit(10).pretty()
+
+        elif number == 4 :
+            songs = mongo.db.songs.find({"years": "2000s"}).sort([('danceability', -1)]).limit(10)
+            return render_template('gallery.html', songs=songs)
+
+        # == == == == == == == == == == == == == == == == == == == == == =
+        # What are the top 5 most pleasant songs across all years?
+        # ----------------------------------------------------------------
+        #  db.songs.find().sort({valence:1}).limit(5).pretty()
+
+        elif number == 5 :
+            songs = mongo.db.songs.find().sort([('valence', 1)]).limit(5)
+            return render_template('gallery.html', songs=songs)
 
 
-@app.route('/about')
-def aboutUs():
-    return render_template("aboutus.html")
+        # if artist_name == "":
+        #     songs = mongo.db.songs.find()
+        #     return render_template('gallery.html', songs=songs)
+        # else:
+        #     songs = mongo.db.songs.find({'artist_name': artist_name})
+        #     songs1 = mongo.db.songs.find({'song_title': artist_name})
+        # return render_template('gallery.html', songs=songs)
 
+@app.route('/year/<yearNumberStr>')
+def yearSearch(yearNumberStr):
 
-@app.route('/contact')
-def getcontactUs():
-    return render_template("contactus.html")
+    yearNumberInt = int(yearNumberStr)
 
+    if yearNumberInt == 1950:
+        songs = mongo.db.sngs.find({'years': '1950s'})
 
-@app.route('/contact', methods=["POST"])
-def postcontactUs():
-    name = request.form['inputName']
-    email = request.form['inputEmail']
-    comments = request.form['textAreaComments']
+    if yearNumberInt == 1960:
+        songs = mongo.db.songs.find({'years': '1960s'})
 
-    data = {
-        "name": name,
-        "email": email,
-        "comments": comments
-    }
+    if yearNumberInt == 1970:
+        songs = mongo.db.songs.find({'years': '1970s'})
 
-    try:
-        mongo.db.contactUs.insert_one(data)
-        return redirect(url_for('all_comments'))
-    except err:
-        return "Inesrt Failed: Something went wrong"
-    # return jsonify(result=data);
+    if yearNumberInt == 1980:
+        songs = mongo.db.songs.find({'years': '1980s'})
 
-    # return render_template("contactResponse.html",result=data)
+    if yearNumberInt == 1990:
+        songs = mongo.db.songs.find({'years': '1990s'})
 
+    if yearNumberInt == 2000:
+        songs = mongo.db.songs.find({'years': '2000s'})
 
-@app.route('/comments')
-def all_comments():
-    comments = mongo.db.contactUs.find()
-    return render_template("comments.html", comments=comments)
-
+    return render_template('gallery.html', songs = songs)
 
 if __name__ == '__main__':
     app.run(debug=True) # I will see any error messages occure due to this true
